17f8de6dadc20fef1ada1babea45aa714624ee70
Harapriya Jena <harapriya@gmail.com>
2025-09-05 09:35:46 +0530

    reusable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
 // import { useAuth } from "../context/AuthContext";
 // import { api } from "../utils/api";
 // import QRScanner from "../components/QRScanner";
